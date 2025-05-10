6e261870aa5cdfe9f0c5aad6a09a9853c69e429e
jarmosz <piotr.jarmosz@o2.pl>
2025-05-10 14:42:02 +0200

    add new2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let email="piotr.jarmosz@o2.pl";
+   // let email="piotr.jarmosz@o2.pl";
+const [email, setEmail] = useState('');
 
     function handleChange(event){
-        console.log(event.target.value);
+       setEmail(event.target.value);}
+
+       let message;
+       if (email.length<5){
+           message= 'Za krótki adres';
+       }else if(email.length>15){
+            message=' Za długi adres';
+        }else {
+           message='Sredni adres';
+
     }
+
+
+
   return (
       <div>
 
         <h1>System do zapisów na zajęcia</h1>
-
         <h2>Twój e-mail to {email}</h2>
+          <div>{message}</div>
           <input type="text"onChange={handleChange}/>
 
       </div>
